18776233fe39985c6f1a565dcbac69e8c28482fa
SolemnSpirit <55107737+SolemnSpirit@users.noreply.github.com>
2019-11-25 22:15:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo "</game>" ;
 echo "</gameList>" ;
 }>>/opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml | echo 50; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 
-sudo curl -o --create-dirs https://raw.githubusercontent.com/SolemnSpirit/test/master/GPFS.dialogrc --output /home/pi/RetroPie/retropiemenu/gpitools/GPFS.dialogrc > /dev/null 2>&1 | echo 75; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
+sudo curl -o https://raw.githubusercontent.com/SolemnSpirit/test/master/GPFS.dialogrc --create-dirs /home/pi/RetroPie/retropiemenu/gpitools/GPFS.dialogrc > /dev/null 2>&1 | echo 75; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 
 sudo curl https://raw.githubusercontent.com/SolemnSpirit/test/master/GPFS.sh --output /home/pi/RetroPie/retropiemenu/gpitools/GPFS.sh > /dev/null 2>&1 | echo 100; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 ) | dialog --hline "  GPi Case Users  " --backtitle "Brought to you by GPi Case Users Group" --gauge "Installing GPFS" 6 50
