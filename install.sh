51ce660e8e83c18441f14d0a39f3ddae9011f3ec
SolemnSpirit <55107737+SolemnSpirit@users.noreply.github.com>
2019-11-25 21:22:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,15 @@
 (
 sudo curl https://raw.githubusercontent.com/SolemnSpirit/test/master/GPFS.png --output /home/pi/RetroPie/retropiemenu/icons/GPFS.png > /dev/null 2>&1 | echo 25; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 
-	sed -i "`wc -l < /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml`i\\<game>\n<path>./gpitools/GPi-Frontend-Switcher/frontend-switcher.sh</path>\n<desc>Easily switch between EmulationStation and Pegasus as your frontend - by SolemnSpirit.</desc>\n<image>./icons/Frontend-Switcher.png</image>\n</game>\\" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml | echo 50; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
+sudo sed -i '$ d' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+echo "<game>" ;	
+echo "<path>./gpitools/GPi-Frontend-Switcher/frontend-switcher.sh</path>";	
+echo "<name>Frontend Switcher</name>";	
+echo "<desc>Easily switch between EmulationStation and Pegasus as your frontend - by SolemnSpirit.</desc>";	
+echo "<image>./icons/GPFS.png</image>";	
+echo "</game>" ;
+echo "</gameList>" ;
+} >>/opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml | echo 50; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 
 sudo curl https://raw.githubusercontent.com/SolemnSpirit/test/master/GPFS.dialogrc --output /home/pi/RetroPie/retropiemenu/gpitools/GPFS.dialogrc > /dev/null 2>&1 | echo 75; echo "XXX"; echo "Installing GPFS"; echo "XXX" ;
 
